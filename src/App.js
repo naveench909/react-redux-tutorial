0dc5cc0b8658ded8347f04c5e7baaa0ca0c36a7a
naveench909 <naveench909@gmail.com>
2021-09-23 02:53:13 -0700

    intial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {BrowserRouter as Router , Switch , Route } from 'react-router-dom';
+import './App.css';
+import Header from './containers/Header';
+import ProductDetails from './containers/ProductDetails';
+import ProductListing from './containers/ProductListing';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={()=>(<ProductListing />)} />
+          <Route path="/product/:productId" exact component={()=>(<ProductDetails />)} />
+          <Route>404 Page Not Found!</Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
